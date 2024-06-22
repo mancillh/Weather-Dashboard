d00e17cf87d77d6141085620cb9bff99c3f0ee81
LeonathaTurtle <mancillh@gmail.com>
2024-06-22 12:06:18 -0500

    created function to make previous search buttons list clickable and functional, modified array indices because the dates weren't all appearing on the six cards,added comments throughout CSS and JS files

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,83 +6,124 @@ const cityInputVal = document.querySelector('#city-input');
 
 // Finds latitude and longitude from user's city input
 function findCoordinates (event) {
+  // prevents page from reloading when user clicks search button
   event.preventDefault();
 
-  let city = cityInputVal.value;
+  // assigns the variable city to the value from the form (what the user enters as their search)
+  let city = cityInputVal.value 
+  // URL to determine longitude and latitude (via Open Weather API) based on user's search input 
   let geocodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`;
-    
+  
+  //fetches longitude and latitude from Open Weather API to pass to getApi function and passes the city name to 
+  //the getApi and addPreviousLocationToList functions
   fetch(geocodingURL)
     .then(function (response) {
         return response.json();
       })
     .then (function (data) {
       getApi(data[0].name, data[0].lat, data[0].lon);
-      addPreviousLocationToList(data[0].name)
+      addPreviousLocationToList(data[0].name);
       })
   };
 
-//adds recently searched cities to a list below search bar
+// Function to add recently searched cities to a list below search bar, also makes each city name a button
 function addPreviousLocationToList (name) {
-    // Create button
+    // Creates button
     const previousLocationBtn = document.createElement('button');
-    // Assign most recently searched city to the button
+
+    // Assigns most recently searched city to the button
     previousLocationBtn.className ='btn previousLocationBtn';
-    // Display the city name
+
+    // Displays the city name
     previousLocationBtn.textContent = name;
-    // Add the city name to a list
+
+    // Adds the city name to a list
     previousLocationList.append(previousLocationBtn);
-    localStorage.setItem('name', name);
-    previousLocationBtn.addEventListener('click', findCoordinates);
+    
+    // Listens for click of one of the grey buttons listed below the search bar, provides name of the city (from 
+    //text on button) to findCoordinatesOfPreviousCity function
+    previousLocationBtn.addEventListener('click', event => {
+      const btn = event.target.closest("button");
+      findCoordinatesOfPreviousCity(`${btn.textContent}`);
+    });
 };
 
-// Pushes latitude and longitude from findCoordinates function into Open Weather Map query and produces the 5 day forecast
-function getApi(name, lat, lon) {
+// Function to find latitude and longitude from city name that appears on previous location button. This function 
+//is nearly identical to the findCoordinates function but activates when the user clicks a button for a recently 
+//searched city instead of from submission of form
+function findCoordinatesOfPreviousCity (previousLocationBtnText) {
+  // Asigns city variable as text on button
+  let city = previousLocationBtnText;
+  // URL to determine longitude and latitude (via Open Weather API) based on button text 
+  let geocodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`;
+  
+  //fetches longitude and latitude from Open Weather API to pass to getApi function and passes the city name to 
+  //the getApi function. Note: the addPreviousLocationToList function is intentionally not called here to prevent
+  //duplicate grey previously searched buttons
+  fetch(geocodingURL)
+    .then(function (response) {
+        return response.json();
+      })
+    .then (function (data) {
+      getApi(data[0].name, data[0].lat, data[0].lon);
+      })
+  };
 
+// Pushes name, latitude and longitude from findCoordinates and findCoordinatesOfPreviousCity functiosn into Open 
+//Weather API query and produces the 5 day forecast
+function getApi(name, lat, lon) {
+  // URL to get 5 day forecast, NOTE: added query for imperial units
   const queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
   
+  //fetches 5 day forecast from Open Weather API and passes city name to the biggest card and the following to all 
+  //weather forecast cards: date, temperature, wind speed, humidity, and an icon to represent the weather. NOTE:
+  //dates must be converted from UTC epoch time (multiply by 1000) and formatted as MM/DD/YYYY using 
+  //toLocaleDateString. Units must be added as string. Icons retrieved were 2x the intended size and needed to be
+  //modified to remove @2x from URL. Icons were sourced from Open Weather API and retrieved from icon code provided
+  //in data array returned by the API fetch
   fetch(queryURL) 
     .then(function (response) {
         return response.json();
     })
     .then (function (data) { 
       document.getElementById("card-title").textContent = name + " " + "(" + new Date(data.list[0].dt*1000).toLocaleDateString() + ")";
-      document.getElementById("temp-text").textContent = "Temp: " + data.list[4].main.temp + " °F";
-      document.getElementById("wind-text").textContent = "Wind: " + data.list[4].wind.speed + " MPH";
-      document.getElementById("humidity-text").textContent = "Humidity: " + data.list[4].main.humidity + " %";
+      document.getElementById("temp-text").textContent = "Temp: " + data.list[0].main.temp + " °F";
+      document.getElementById("wind-text").textContent = "Wind: " + data.list[0].wind.speed + " MPH";
+      document.getElementById("humidity-text").textContent = "Humidity: " + data.list[0].main.humidity + " %";
       const img = document.getElementById("icon");
-      let iconCode = data.list[4].weather[0].icon;
+      let iconCode = data.list[0].weather[0].icon;
       img.setAttribute('src',`https://openweathermap.org/img/wn/${iconCode}.png`);
 
-      document.getElementById("card-title-1").textContent = new Date(data.list[12].dt*1000).toLocaleDateString();
-      document.getElementById("temp-text-1").textContent = "Temp: " + data.list[12].main.temp + " °F";
-      document.getElementById("wind-text-1").textContent = "Wind: " + data.list[12].wind.speed + " MPH";
-      document.getElementById("humidity-text-1").textContent = "Humidity: " + data.list[12].main.humidity + " %";
+      document.getElementById("card-title-1").textContent = new Date(data.list[8].dt*1000).toLocaleDateString();
+      document.getElementById("temp-text-1").textContent = "Temp: " + data.list[8].main.temp + " °F";
+      document.getElementById("wind-text-1").textContent = "Wind: " + data.list[8].wind.speed + " MPH";
+      document.getElementById("humidity-text-1").textContent = "Humidity: " + data.list[8].main.humidity + " %";
       const img1 = document.getElementById("icon-1");
-      let iconCode1 = data.list[12].weather[0].icon;
+      let iconCode1 = data.list[8].weather[0].icon;
       img1.setAttribute('src',`https://openweathermap.org/img/wn/${iconCode1}.png`);
 
-      document.getElementById("card-title-2").textContent = new Date(data.list[20].dt*1000).toLocaleDateString();
-      document.getElementById("temp-text-2").textContent = "Temp: " + data.list[20].main.temp + " °F";
-      document.getElementById("wind-text-2").textContent = "Wind: " + data.list[20].wind.speed + " MPH";
-      document.getElementById("humidity-text-2").textContent = "Humidity: " + data.list[20].main.humidity + " %";
+      document.getElementById("card-title-2").textContent = new Date(data.list[16].dt*1000).toLocaleDateString();
+      document.getElementById("temp-text-2").textContent = "Temp: " + data.list[16].main.temp + " °F";
+      document.getElementById("wind-text-2").textContent = "Wind: " + data.list[16].wind.speed + " MPH";
+      document.getElementById("humidity-text-2").textContent = "Humidity: " + data.list[16].main.humidity + " %";
       const img2 = document.getElementById("icon-2");
-      let iconCode2 = data.list[20].weather[0].icon;
+      let iconCode2 = data.list[16].weather[0].icon;
       img2.setAttribute('src',`https://openweathermap.org/img/wn/${iconCode2}.png`);
 
-      document.getElementById("card-title-3").textContent = new Date(data.list[28].dt*1000).toLocaleDateString();
-      document.getElementById("temp-text-3").textContent = "Temp: " + data.list[28].main.temp + " °F";
-      document.getElementById("wind-text-3").textContent = "Wind: " + data.list[28].wind.speed + " MPH";
-      document.getElementById("humidity-text-3").textContent = "Humidity: " + data.list[28].main.humidity + " %";
+      document.getElementById("card-title-3").textContent = new Date(data.list[24].dt*1000).toLocaleDateString();
+      document.getElementById("temp-text-3").textContent = "Temp: " + data.list[24].main.temp + " °F";
+      document.getElementById("wind-text-3").textContent = "Wind: " + data.list[24].wind.speed + " MPH";
+      document.getElementById("humidity-text-3").textContent = "Humidity: " + data.list[24].main.humidity + " %";
       const img3 = document.getElementById("icon-3");
-      let iconCode3 = data.list[28].weather[0].icon;
+      let iconCode3 = data.list[24].weather[0].icon;
       img3.setAttribute('src',`https://openweathermap.org/img/wn/${iconCode3}.png`);
 
-      document.getElementById("card-title-4").textContent = new Date(data.list[36].dt*1000).toLocaleDateString();
-      document.getElementById("temp-text-4").textContent = "Temp: " + data.list[36].main.temp + " °F";
-      document.getElementById("wind-text-4").textContent = "Wind: " + data.list[36].wind.speed + " MPH";
-      document.getElementById("humidity-text-4").textContent = "Humidity: " + data.list[36].main.humidity + " %";
+      document.getElementById("card-title-4").textContent = new Date(data.list[32].dt*1000).toLocaleDateString();
+      document.getElementById("temp-text-4").textContent = "Temp: " + data.list[32].main.temp + " °F";
+      document.getElementById("wind-text-4").textContent = "Wind: " + data.list[32].wind.speed + " MPH";
+      document.getElementById("humidity-text-4").textContent = "Humidity: " + data.list[32].main.humidity + " %";
       const img4 = document.getElementById("icon-4");
-      let iconCode4 = data.list[36].weather[0].icon;
+      let iconCode4 = data.list[32].weather[0].icon;
       img4.setAttribute('src',`https://openweathermap.org/img/wn/${iconCode4}.png`);
 
       document.getElementById("card-title-5").textContent = new Date(data.list[39].dt*1000).toLocaleDateString();
@@ -93,10 +134,10 @@ function getApi(name, lat, lon) {
       let iconCode5 = data.list[39].weather[0].icon;
       img5.setAttribute('src',`https://openweathermap.org/img/wn/${iconCode5}.png`);
 
+      // Clears search (form)
       document.getElementById("city-form").reset(); 
     });
   };
 
+// Activates findCoordinates function on click of search button
 searchButton.addEventListener('submit', findCoordinates);
-
-
